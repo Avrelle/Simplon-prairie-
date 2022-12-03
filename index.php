97360574d42283100a422c4e7f1e30f53b0f5c4f
Avrelle <anto.delahaye@gmail.com>
2022-12-03 17:35:22 +0100

    création de la page mon compte pro

--- index.php
+++ index.php
@@ -4,9 +4,60 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/main.css">
     <title>Document</title>
 </head>
 <body>
-    <h1>Hello worldzedsq</h1>
+    <header>
+            <nav class="navbar-part">
+                <div class="container">
+                    <div class="navbar-content">
+                        <a href="index.html">
+                            <img src="./images/gamelle logo.png" alt="">
+                        </a>
+                        <div class="navbar-links">
+                            <ul class="navbar-link">
+                                <a href="index.html">
+                                    <li class="navbar-link1">S'inscrire</li>
+                                </a>
+                            </ul>
+
+                        </div>
+                    </div>
+                </div>
+
+            </nav>
+    </header>
+<main>
+    <section class="connexion">
+        <div class="form">
+            <div class="form_content">
+            
+                <div class="title_form">
+                    <h1>Mon compte pro</h1>
+                </div>
+
+                <form id="connexion">
+                    <label for="User">Nom d'utilisateur :</label><br>
+                    <input type="text" id="User" name="User"><br>
+                    <div class="p">
+                    <label for="MDP">Mot de passe :</label><br>
+                    <input type="password" id="MDP" name="MDP"><br>
+                    </div>
+                </form>
+                <div class="button_form">
+                    <button type="submit" form="connexion" value="Submit">Connexion</button>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <footer>
+    <div class="footer">
+        <p>Croquette n’est pas un service d’urgence. En cas d’urgence appelez le 3115.</p>
+        </div>    
+ </footer>
+ </main>
 </body>
-</html>
+
