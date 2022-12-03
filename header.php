59959cc5f86ff5321a4a9adb52a1db73fc28ebe4
Avrelle <anto.delahaye@gmail.com>
2022-12-03 16:39:40 +0100

    seconde  commit

---
+++ header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/main.css">
+    <title>Document</title>
+</head>
+<body>
+   
+<body>
+    <header>
+        <nav class="navbar-part">
+            <div class="container">
+                <div class="navbar-content">
+                    <a href="index.html">
+                        <img src="./images/gamelle logo.png" alt="">
+                    </a>
+                    <div class="navbar-links">
+                        <ul class="navbar-link">
+                            <a href="index.html">
+                                <li class="navbar-link1">S'inscrire</li>
+                            </a>
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
+
+        </nav>
+    </header>
+</body>
+</html>
