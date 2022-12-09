d6cbc37c4ad6396eec32cf2bb764bfc4e606d3e7
Avrelle <anto.delahaye@gmail.com>
2022-12-09 10:31:59 +0100

    modication de l'inscription

--- inscription.php
+++ inscription.php
@@ -1,6 +1,15 @@
 <?php
     session_start();
-   $bdd = new PDO('mysql:host=localhost;port=8080;dbname=Veto_app;charset=utf8', 'root', 'root');
+   
+    try {
+        $bdd = new PDO('mysql:host=localhost;port=8080;dbname=Veto_app;charset=utf8', 'root', 'root');
+   $bdd->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    
+    }catch(PDOException$e){
+        print"Erreur!:".$e->getMessage()."<br/>";
+        die();
+    }
+   
    
  if(isset($_POST['envoi'])){
     if(!empty($_POST['user']) && !empty($_POST['email']) && !empty($_POST['city']) && !empty($_POST['birth_date'])
@@ -10,8 +19,14 @@
         $city = htmlspecialchars($_POST['city']);
         $birth_date = htmlspecialchars($_POST['birth_date']);
         $password = sha1($_POST['password']);
-        $insertUser = $bdd->prepare('INSERT INTO client (user, email, city, birth_date, password) VALUES (?,?,?,?,?)');
-        $insertUser-> execute(array($user, $email, $city, $birth_date, $password));
+        $sql = 'INSERT INTO client (user, email, city, birth_date, password) VALUES (:user, :email , :city , :birth_date , :password)';
+        $query = $bdd->prepare($sql);
+        $query->bindValue(':user', $user, PDO::PARAM_STR);
+            $query->bindValue(':email', $email, PDO::PARAM_STR);
+            $query->bindValue(':city', $city, PDO::PARAM_STR);
+            $query->bindValue(':birth_date', $birth_date, PDO::PARAM_STR);
+            $query->bindValue(':password', $password, PDO::PARAM_STR);
+        $query->execute();
         
         $recupUser = $bdd-> prepare('SELECT * FROM client WHERE user = ? AND email = ? AND city = ? AND birth_date = ? AND password = ?');
         $recupUser->execute(array($user, $email, $city, $birth_date, $password));
